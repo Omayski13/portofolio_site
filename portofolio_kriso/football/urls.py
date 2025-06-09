ba2896b60f919adf94c9445bef7969c7038a9ec9
Omayski13 <k.omayski@abv.bg>
2025-06-09 22:16:33 +0300

    chore: created football page with main, about and certificates sections

--- portofolio_kriso/football/urls.py
+++ portofolio_kriso/football/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    # path('', ),
+from portofolio_kriso.football.views import FootballPageView
 
+urlpatterns = [
+    path('', FootballPageView.as_view(), name='football'),
 ]
