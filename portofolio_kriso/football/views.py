67b8f8533a0e10ff53f05f475aebf4fd11884047
Kristiyan Omayski <122819613+Omayski13@users.noreply.github.com>
2025-06-10 15:54:48 +0300

    fix: updated football about and created calendar

--- portofolio_kriso/football/views.py
+++ portofolio_kriso/football/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .google_auth import authenticate_google
+from googleapiclient.discovery import build
 
 
 # Create your views here.
 
 class FootballPageView(TemplateView):
     template_name = 'football.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+
+    #     creds = authenticate_google()
+    #     service = build('calendar', 'v3', credentials=creds)
+
+    #     events_result = service.events().list(
+    #         calendarId='primary',
+    #         timeMin='2025-06-10T00:00:00Z',
+    #         maxResults=10,
+    #         singleEvents=True,
+    #         orderBy='startTime'
+    #     ).execute()
+        
+    #     context['events'] = events_result.get('items', [])
+    #     return context
